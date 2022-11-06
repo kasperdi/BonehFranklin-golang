89305461cf3cd779a13df666e5a7c2c34f66963f
kasperdi <kasperdi1412@gmail.com>
2022-11-06 16:32:02 +0100

    Added benchmarking data and python code for plotting

---
+++ Benchmarking/generate_plots.py
@@ -0,0 +1,17 @@
+import matplotlib.pyplot as plt
+
+# creating the dataset
+data = {'IBE - Setup':1.571673, 'IBE - Extract':0.792551, 'IBE - Encrypt':8.947755,
+        'IBE - Decrypt':5.904935}
+
+functions = list(data.keys())
+runtimes = list(data.values())
+  
+fig = plt.figure(figsize = (10, 5))
+ 
+# creating the bar plot
+plt.bar(functions, runtimes, color ='royalblue')
+ 
+plt.ylabel("Miliseconds")
+plt.title("Benchmarking of IBE implementation")
+plt.show()
